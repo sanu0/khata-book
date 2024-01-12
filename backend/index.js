4331df4846c82556ec545a2ddf1862bf947bd71b
sanu0 <kumarsanu3703@gmail.com>
2024-01-12 14:49:29 +0530

    Add Zod library

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const {Balance , Expenses } = require("./db");
+const {updateBalance , addExpense} = require("./types");
 const port = 3000;
 
 const app = express();
@@ -8,7 +9,27 @@ const app = express();
 app.use(express.json());
 
 
+function validateInputUpdateBalance(obj){
+    const updateBalanceSchema = updateBalance;
+    const response = updateBalanceSchema.safeParse(obj);
+    //console.log(response);
+    if(response.success){
+        return obj;
+    }else{
+        return false;
+    }
+}
 
+function validateInputUpdateExpense(obj){
+    const updateExpenseSchema = addExpense;
+    const response = updateExpenseSchema.safeParse(obj);
+    //console.log(response);
+    if(response.success){
+        return obj;
+    }else{
+        return false;
+    }
+}
 
 app.get('/balance', async function (req, res) {
     //Give you the available balnce in your khata
@@ -22,21 +43,27 @@ app.get('/balance', async function (req, res) {
 
 app.post('/balance', async (req, res) => {
     const amount = req.body.amount;
-    try{
-        await Balance.updateOne({
-            //id : '65a02f8fbe25ed57a36551c7'
-        },{
-            amount : amount
+    if(!validateInputUpdateBalance(amount)){
+        res.status(411).json({
+            msg : "Input is invalid"
         })
-        res.status(200).json({
-            msg : "Balance added"
-        })
+    }else{
+        try{
+            await Balance.updateOne({
+                //id : '65a02f8fbe25ed57a36551c7'
+            },{
+                amount : amount
+            })
+            res.status(200).json({
+                msg : "Balance added"
+            })
 
-    }catch(e){
-        console.log(e);
-        res.json({
-            msg : "Something wrong!"
-        })
+        }catch(e){
+            console.log(e);
+            res.json({
+                msg : "Something wrong!"
+            })
+        }
     }
 })
 app.get('/expense', async (req, res) => {
@@ -49,28 +76,35 @@ app.post('/expense', async (req, res) => {
     const amount = req.body.price;
     const description = req.body.description;
     const isRecieved = req.body.isRecieved;
-    try{
-        const resp = await Expenses.create({
-            amount,
-            description,
-            isRecieved
-        })
-        //console.log(resp.amount);
-        if(isRecieved){
-            await Balance.updateOne({},{
-                "$inc" : {amount : resp.amount}
-            })
-        }else{
-            await Balance.updateOne({},{
-                "$inc" : {amount : -resp.amount}
-            })
-        }
-        res.status(200).json({
-            msg : "expenses added"
-        })
 
-    }catch(e){
-        console.log(e);
+    if(!validateInputUpdateExpense({amount,description,isRecieved})){
+        res.status(411).json({
+            msg : "Input is invalid"
+        })
+    }else{
+        try{
+            const resp = await Expenses.create({
+                amount,
+                description,
+                isRecieved
+            })
+            //console.log(resp.amount);
+            if(isRecieved){
+                await Balance.updateOne({},{
+                    "$inc" : {amount : resp.amount}
+                })
+            }else{
+                await Balance.updateOne({},{
+                    "$inc" : {amount : -resp.amount}
+                })
+            }
+            res.status(200).json({
+                msg : "expenses added"
+            })
+
+        }catch(e){
+            console.log(e);
+        }
     }
     
 
